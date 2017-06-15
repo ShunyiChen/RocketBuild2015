1c9e8715671bd7e1517341e470d44a8359bc9b07
ShunyiChen <526637060@qq.com>
2017-06-15 10:14:57 +0800

    Commit it to my own branch.
    Added some constants.

---
+++ src/main/java/com/rocket/tms/util/JavaClass.java
@@ -0,0 +1,17 @@
+package com.rocket.tms.util;
+
+public class JavaClass {
+	public static String BASE_HOST = "https://modeloffice.trubiquity.net:8383";
+	public static String DOWNLOAD_FOLDER = "TCDOWNLOADFOLDER";
+	public static String TC_UPLOAD_FOLDER = "TCUPLOADFOLDER";
+	
+	public static String FILE_ENCRY_FOLDER = "C:/tpa_encry_folder";
+	public static String IDENTITY_STUB_URL = BASE_HOST + "/Identity/api/identity";
+	public static String COMMUNITY_BASE_URL = BASE_HOST + "/Identity/api/community/";
+	public static String TRUSHARE_IDENTITY_URL = BASE_HOST + "/Identity/api/identity/";
+	public static String TRUSHARE_BASE_URL = BASE_HOST + "/Trushare/api";
+	public static String IDENTITY_BASE_URL = BASE_HOST + "/Identity/api";
+	public static String PROFILE_BASE_URL = BASE_HOST + "/Identity/api/profile/";
+	public static String TRUCORE_STORE_BASE_URL = BASE_HOST + "/Identity/api/identity";
+	public static String MFX_BASE_URL = BASE_HOST + "/mfx/api/mfx/";
+}
